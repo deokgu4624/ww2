6c205a2e4c09d093d18655924c4ce8be33e21f58
DESKTOP-8AT52HP\namhy <deokgu4624@gmail.com>
2021-02-25 02:07:06 +0900

    .

--- js/script.js
+++ js/script.js
@@ -1,36 +1,64 @@
-//랜덤 이미지
-var front_image = document.getElementsByClassName("front-image");
-var i;
-var j;
-var num = [];
-for(i=0; i<front_image.length; i++){
-    num[i] = Math.floor(Math.random()*front_image.length)+1;
-    console.log(num);
-    front_image[i].id = "front-image"+num[i];
-    for(j=0; j<num.length-1; j++){
-        console.log("j"+j);
-        if(num[i]===num[j]){
-            console.log("same");
-            num.pop();
-            i--;
-        }
-    }
-}
-//이미지 이동
-var targetPos;
-function moveleft(){    
-    for(i=0; i<front_image.length; i++){
-        var targetPos = front_image[i].getBoundingClientRect().left;
-        targetPos -= 1;
-        front_image[i].style.left = (targetPos/window.innerWidth)*100 + "vw";
-        if((targetPos/window.innerWidth)*100<-320){
-            front_image[i].style.left = 100 + "vw";
-        }
-    }
-    console.log(front_image[0].getBoundingClientRect().left)
+var circle = document.getElementById("whitecircle");
+var redbox = document.getElementById("redbox");
+var title = document.getElementById("title");
+var x = 1;
+
+circle.addEventListener("click", forward);
+title.addEventListener("click", back);
+
+function forward(){
+    redbox.style.animationName = "redbox"+x;
+    redbox.style.animationPlayState = "paused";
+    setTimeout(running, 1000);
+    x+=1;
+    console.log(x);
 }
 
-setInterval(moveleft, 1000/10);
+function back(){
+    x-=1;
+    redbox.style.animationName = "redbox"+"-"+x;
+    redbox.style.animationPlayState = "paused";
+    setTimeout(running, 1000);
+    
+    console.log(x);
+}
+function running(){
+    redbox.style.animationPlayState = "running";
+}
+
+// //랜덤 이미지
+// var front_image = document.getElementsByClassName("front-image");
+// var i;
+// var j;
+// var num = [];
+// for(i=0; i<front_image.length; i++){
+//     num[i] = Math.floor(Math.random()*front_image.length)+1;
+//     console.log(num);
+//     front_image[i].id = "front-image"+num[i];
+//     for(j=0; j<num.length-1; j++){
+//         console.log("j"+j);
+//         if(num[i]===num[j]){
+//             console.log("same");
+//             num.pop();
+//             i--;
+//         }
+//     }
+// }
+// //이미지 이동
+// var targetPos;
+// function moveleft(){    
+//     for(i=0; i<front_image.length; i++){
+//         var targetPos = front_image[i].getBoundingClientRect().left;
+//         targetPos -= 1;
+//         front_image[i].style.left = (targetPos/window.innerWidth)*100 + "vw";
+//         if((targetPos/window.innerWidth)*100<-320){
+//             front_image[i].style.left = 100 + "vw";
+//         }
+//     }
+//     console.log(front_image[0].getBoundingClientRect().left)
+// }
+
+// setInterval(moveleft, 1000/10);
 
 
 // var t;
